d29a5f9111ab03972345a3804c142898566b4958
Akinselure Abiola <akinselureabiola@Akinselures-MacBook-Pro.local>
2026-05-25 14:05:33 +0200

    Added tester accounts

--- create_user.py
+++ create_user.py
@@ -1,15 +1,24 @@
 import bcrypt
 
-passwords = [
-    "Test123",
-    "Test123",
-    "Test123"
-]
+users = {
+    "admin": "Admin123",
+    "bankole": "Test123",
+    "ariyo": "Test123",
+    "temitayo": "Test123",
+    "oluwaseyi": "Test123",
+    "ayodeji": "Test123",
+    "daizsign": "Test123",
+    "ahmed": "Test123"
+}
 
-for password in passwords:
+print("\nGenerated Password Hashes:\n")
+
+for username, password in users.items():
     hashed = bcrypt.hashpw(
         password.encode(),
         bcrypt.gensalt()
     )
 
+    print(f"{username}:")
     print(hashed.decode())
+    print("-" * 50)
